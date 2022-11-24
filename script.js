a015284e5227ee9a8e78085486d0663f74ef1dc3
andrea <benedetti.a93@gmail.com>
2022-11-24 12:44:22 +0100

    Uploading my exercise
    I am uploading everything for the first time: index, style, javascript, resources

---
+++ script.js
@@ -0,0 +1,30 @@
+// load our data <- d3.csv d3.svg d3.html d3.tsv d3.json
+// bind our data to the html page
+// visualize things!!!
+
+let data = d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRRy2dEW3gKEE-oWeDQUG3awj9-QFWm5XXUeTELWG3rILfFRrkEDglmGgTOebVuD8LY5Q8QihNvgU3H/pub?gid=0&single=true&output=csv");
+
+let cardsContainer = d3.select("#visualization-container");
+
+data.then(function(myData) {
+    console.log(myData);
+
+    let card = cardsContainer.selectAll("div")
+    .data(myData)
+    .join("div")
+    .attr("class", "card");
+
+    card.append("div")
+    .attr("class", "card-header")
+    .text(function(datum) {
+        return datum.timestamp;
+    });
+
+    card.append("img")
+    .attr("class", "card-img-top")
+    .attr("src", function(datum) {
+        let path = "assets/images/";
+        let filename = datum.filename;
+        return path + filename;
+    })
+});
